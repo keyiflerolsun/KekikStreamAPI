6fed659c1619b78974bc901f4deb96c18ba2ea27
keyiflerolsun <keyiflerolsun@gmail.com>
2025-12-17 01:25:01 +0300

    🕊
    🕊

--- Core/Modules/__init__.py
+++ Core/Modules/__init__.py
@@ -2,7 +2,7 @@
 
 from CLI        import konsol
 from fastapi    import FastAPI
-from contextlib import asynccontextmanager, suppress
+from contextlib import asynccontextmanager
 from curl_cffi  import AsyncSession
 from Settings   import AVAILABILITY_CHECK
 from Public.API.v1.Libs import plugin_manager
@@ -16,11 +16,15 @@ async def lifespan(app: FastAPI):
         async with AsyncSession(impersonate="chrome", timeout=3) as oturum:
             for name in plugin_manager.get_plugin_names():
                 plugin = plugin_manager.select_plugin(name)
-                with suppress(Exception):
+                try:
                     istek = await oturum.get(plugin.main_url)
                     if istek.status_code != 200:
                         plugin_manager.plugins.pop(name)
                         konsol.log(f"[red]Eklentiye erişilemiyor : {plugin.name} | {plugin.main_url}")
+                except Exception:
+                    plugin_manager.plugins.pop(name)
+                    konsol.log(f"[red]Eklentiye erişilemiyor : {plugin.name} | {plugin.main_url}")
+
         konsol.log(f"[green]Eklenti erişim kontrolleri tamamlandı.")
 
     yield
